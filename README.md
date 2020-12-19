de0b75bb0beb63f835eff14edb5883d7cf7c4826
panxiao81 <pan-xiao@live.cn>
2020-12-19 13:24:07 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# clash-subscribe-script
+# Clash Subscribe Script
+
+一个自用的 Clash 订阅更新脚本
+
+## 使用方法
+
+安装依赖
+
+```shell
+pip3 install ruamel.yaml requests
+```
+
+脚本会从标准输入读入 `URL`, 并将修改好的 YAML 文档送至标准输出。
+
+```shell
+echo "your_sub_url_here" | python3 main.py > config.yml
+```
+
+目前仅有写死的添加 `socks-port` 与 `redir-port` 的功能
+
+理论上可以方便的对功能进行扩展，对字典与列表进行插入与赋值操作即可。
+
+可以写进 `crontab` 来达到自动更新订阅的目的, 如：
+
+```console
+$ crontab -e -u clash
+
+# 输入
+
+0 4 * * * echo "your_sub_url_here" | python3 /path/to/main.py > /path/to/config.yml
+```
+
+进行每日 4 点更新订阅。
---
+++ main.py
@@ -0,0 +1,36 @@
+# A clash subscribe with modify script
+# Writing by Pan Xiao 2020-12-19
+# Version 0.1
+# Please use
+# pip3 install ruamel.yaml requests
+# First
+from ruamel import yaml
+import requests
+
+# Get origin sub file
+url = input()
+clash_sub_origin = requests.get(url)
+clash_sub_origin.raise_for_status()
+
+if clash_sub_origin.status_code == requests.codes.ok:
+    # load yaml data
+    clash_sub_data = yaml.load(clash_sub_origin.text, Loader=yaml.RoundTripLoader)
+    # 维护一个列表，使字典有序
+    clash_sub_key = list(clash_sub_data.keys())
+
+    # Add keys and value
+    clash_sub_key.insert(clash_sub_key.index('mixed-port') + 1, 'socks-port')
+    clash_sub_data['socks-port'] = 7891
+
+    # add redir port
+    clash_sub_key.insert(clash_sub_key.index('socks-port') + 1, 'redir-port')
+    clash_sub_data['redir-port'] = 7892
+
+    # rebuild dict
+    clash_sub_output = {}
+    for i in clash_sub_key:
+        clash_sub_output[i] = clash_sub_data[i]
+
+    # print(clash_sub_output)
+    print(yaml.round_trip_dump(clash_sub_output, allow_unicode=True))
+

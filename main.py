625180a0375ed9db8c9bfe0b43caaa4843cdfaa3
panxiao81 <pan-xiao@live.cn>
2020-12-24 00:14:26 +0800

    修改方式打包函数

--- main.py
+++ main.py
@@ -7,6 +7,15 @@
 from ruamel import yaml
 import requests
 
+
+def add_key_value(key_name: str, locate: int, list_name: list, dict_name: dict, value: object):
+    if key_name in dict_name:
+        dict_name[key_name] = value
+    else:
+        list_name.insert(locate, key_name)
+        dict_name[key_name] = value
+
+
 # Get origin sub file
 url = input()
 clash_sub_origin = requests.get(url)
@@ -19,12 +28,14 @@ if clash_sub_origin.status_code == requests.codes.ok:
     clash_sub_key = list(clash_sub_data.keys())
 
     # Add keys and value
-    clash_sub_key.insert(clash_sub_key.index('mixed-port') + 1, 'socks-port')
-    clash_sub_data['socks-port'] = 7891
+    # clash_sub_key.insert(clash_sub_key.index('mixed-port') + 1, 'socks-port')
+    # clash_sub_data['socks-port'] = 7891
+    add_key_value('socks-port', clash_sub_key.index('mixed-port') + 1, clash_sub_key, clash_sub_data, 7891)
 
     # add redir port
-    clash_sub_key.insert(clash_sub_key.index('socks-port') + 1, 'redir-port')
-    clash_sub_data['redir-port'] = 7892
+    # clash_sub_key.insert(clash_sub_key.index('socks-port') + 1, 'redir-port')
+    # clash_sub_data['redir-port'] = 7892
+    add_key_value('redir-port', clash_sub_key.index('socks-port') + 1, clash_sub_key, clash_sub_data, 7892)
 
     # rebuild dict
     clash_sub_output = {}
@@ -33,4 +44,3 @@ if clash_sub_origin.status_code == requests.codes.ok:
 
     # print(clash_sub_output)
     print(yaml.round_trip_dump(clash_sub_output, allow_unicode=True))
-
